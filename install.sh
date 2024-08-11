3e7daaba934e09ac4a2ca65122c6598bda3da815
Tatsuhiro Ikebe <beike315@icloud.com>
2024-08-11 20:56:12 +0900

    chore: add source ros setup.bash

---
+++ install.sh
@@ -0,0 +1,11 @@
+git clone git@github.com:Shinsotsu-Tsukuba-Challenger/trainee.git $HOME/trainee
+grep -q "source $HOME/trainee/install/setup.bash" $HOME/.bashrc || echo "source $HOME/trainee/install/setup.bash" >> $HOME/.bashrc
+grep -q "export TRAINEE_WS=$HOME/trainee" $HOME/.bashrc || echo "export TRAINEE_WS=$HOME/trainee" >> $HOME/.bashrc
+source $HOME/.bashrc
+cd $TRAINEE_WS && mkdir src
+vcs import src < trainee.repos --debug
+rosdep update
+rosdep install -y --from-paths src --ignore-src --rosdistro $ROS_DISTRO
+source /opt/ros/$ROS_DISTRO/setup.bash
+colcon build --symlink-install
+source $HOME/.bashrc
