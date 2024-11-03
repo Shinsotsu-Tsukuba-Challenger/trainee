cc7c07bf1a4cc7446088269aa6523c5a7fd6f24c
Tatsuhiro Ikebe <beike315@icloud.com>
2024-11-03 14:31:24 +0900

    fix: add rosdep install option skip keys odrive_ros2_control (#16)

--- setup.sh
+++ setup.sh
@@ -8,19 +8,24 @@ grep -q "source $HOME/trainee/install/setup.bash" $HOME/.bashrc || echo "source 
 grep -q "export TRAINEE_WS=$HOME/trainee" $HOME/.bashrc || echo "export TRAINEE_WS=$HOME/trainee" >> $HOME/.bashrc
 source $HOME/.bashrc
 cd $TRAINEE_WS && mkdir src
+
 if [ "$1" == "pc" ]; then
     echo "Setting up for PC"
     vcs import src < trainee_pc.repos --debug
+    sudo apt update -y
+    rosdep update
+    rosdep install -y --from-paths src --skip-keys odrive_ros2_control --ignore-src --rosdistro $ROS_DISTRO
 elif [ "$1" == "raspi" ]; then
     echo "Setting up for Raspberry Pi"
     vcs import src < trainee_raspi.repos --debug
+    sudo apt update -y
+    rosdep update
+    rosdep install -y --from-paths src --ignore-src --rosdistro $ROS_DISTRO
 else
     echo "No or incorrect argument provided. Default setup will be used."
     vcs import src < trainee.repos --debug
 fi
-sudo apt update -y
-rosdep update
-rosdep install -y --from-paths src --ignore-src --rosdistro $ROS_DISTRO
+
 source /opt/ros/$ROS_DISTRO/setup.bash
 colcon build --symlink-install
 source $HOME/.bashrc
