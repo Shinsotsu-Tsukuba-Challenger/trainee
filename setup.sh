ea42358f47d6dd8d0b94c2040a40e29a383302be
Tatsuhiro Ikebe <tatsuhiro.ikebe@gmail.com>
2025-06-01 20:38:34 +0900

    fix: rsync path

--- setup.sh
+++ setup.sh
@@ -36,13 +36,11 @@ if [ "$CACHE" == "true" ] && [ -d "$CACHE_PATH" ]; then
     for pkg in $(ls src); do
         echo "Checking package: $pkg"
         cd "src/$pkg"
-        ls -a $CACHE_PATH
-        ls -a $CACHE_PATH/$pkg/
         local_commit=$(git rev-parse HEAD)
         cache_commit=$(git --git-dir="$CACHE_PATH/$pkg/.git" rev-parse HEAD)
         if [ "$local_commit" == "$cache_commit" ]; then
             echo "Commit match for $pkg. Syncing..."
-            rsync -a --delete "$CACHE_PATH/$pkg/" "src/$pkg/"
+            rsync -a --delete "$CACHE_PATH/$pkg/" "$TRAINEE_WS/src/$pkg/"
         else
             echo "Commit mismatch for $pkg. Skipping rsync."
         fi
